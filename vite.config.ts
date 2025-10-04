bff5b958da8fb4bceb44a0e488534eee5cb020cf
James <james789111@gmail.com>
2025-10-04 09:59:00 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: string) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react'
-            if (id.includes('@chakra-ui')) return 'vendor-chakra'
-            if (id.includes('firebase')) return 'vendor-firebase'
-            if (id.includes('recharts')) return 'vendor-recharts'
-            return 'vendor'
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1200,
-  },
 })
